4dd150d36e593afc2bf239d63c4bf0a67a3a145c
James Kaviyil Jose <jamesjose03@gmail.com>
2020-02-08 21:52:01 +0530

    feat: MongoDB user creation and validation done

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const authRouter = require('./routes/auth-router')
 const passportSetup = require('./config/passport-config')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 
 const app = express()
 
 app.set('view engine','ejs')
+
+mongoose.connect(keys.mongodb.dbURI, ()=> {
+    console.log('connect to mongodb successful!')
+})
+
 app.use('/auth',authRouter)
 app.get('/',(req,res) => {
     res.render('home')
--- config/passport-config.js
+++ config/passport-config.js
@@ -1,6 +1,8 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
 const keys = require('./keys')
+const User = require('../models/user-model')
+
 passport.use(
     new GoogleStrategy({
     //options for the strategy
@@ -8,8 +10,19 @@ passport.use(
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret 
 }, (accessToken, refreshToken, profile, done) => {
-    //callback fn
-    console.log('callback fn fired')
-    console.log(profile)
+    //Profile exists or not
+   User.findOne({googleID: profile.id}).then((currentUser) =>{
+        if(currentUser){
+            console.log('user is ',currentUser)
+        } else {
+            new User({
+                username: profile.displayName,
+                googleID: profile.id
+            }).save().then((newUser) => {
+                console.log('new user created: '+ newUser)
+            })
+        }
+   })
+ 
 })
 )
